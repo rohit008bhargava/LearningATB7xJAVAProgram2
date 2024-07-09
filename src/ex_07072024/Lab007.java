c2f26d74b12e7100114347441092807abd5c5f8b
Rohit <rohit008bhargava@gmail.com>
2024-07-10 05:22:16 +0530

    All files Added

--- src/ex_07072024/Lab007.java
+++ src/ex_07072024/Lab007.java
@@ -1,7 +1,8 @@
-package ex_07062024;
+package ex_07072024;
 
 public class Lab007 {
     public static void main(String[] args) {
         System.out.println("Lab007");
+        System.out.println("Lab008");
     }
 }
