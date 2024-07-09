a67ad5feb085e33191abf6fafe6b3d26d127e426
Rohit <rohit008bhargava@gmail.com>
2024-07-09 19:00:35 +0530

    Lab007 fix

---
+++ src/ex_07062024/Lab003.java
@@ -0,0 +1,13 @@
+package ex_07062024;
+
+public class Lab003 {
+
+    public static void main(String[] args) {
+        // Single comment - This code will not run or executed
+        // 2+2 ?
+        //dsdsdsdsdsds
+
+
+    }
+
+}
---
+++ src/ex_07062024/Lab004.java
@@ -0,0 +1,15 @@
+package ex_07062024;
+
+public class Lab004 {
+
+    public static void main(String[] args) {
+
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(3/2);
+
+    }
+
+}
