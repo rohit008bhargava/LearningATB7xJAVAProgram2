7d795bca1f3f4e49adf4e182785a68e7cbaef31f
Rohit <rohit008bhargava@gmail.com>
2024-07-17 07:01:46 +0530

    13 july 2024

---
+++ src/ex_13072024/Lab015.java
@@ -0,0 +1,48 @@
+package ex_13072024;
+
+public class Lab015 {
+    public static void main(String[] args) {
+
+        // data_type variable_name = variable_value(literal)
+
+        // Data Type
+        // Primitive Data Type
+        // boolean
+           // true or false
+        boolean is_married = true;
+        boolean are_you_a_dog = false;
+        System.out.println(is_married);
+        System.out.println(are_you_a_dog);
+
+
+        // Numerical
+           // Integral - byte, short, int, long
+
+        byte b = 100;
+        short s = 101;
+        int age = 130;
+        long phone_number = 8860925295l;
+
+
+           // char - char  - A,B
+        char grade = 'A';
+
+        // floating
+           // float
+        float pi = 3.14f;
+           // double
+        double d = 118.654;
+
+
+        // Non primitive Data Types
+        // string  name -> "pramod" -, number or char or bunch of char
+        String name = "Pramod";
+        String name2 = "P";
+//        char cc = "c";
+
+
+
+
+
+    }
+}
